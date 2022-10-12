ce18c89a15bd6d7ee2063377eafccf7e2067410b
Peter Tvingmark <tvingmark@gmail.com>
2022-10-12 21:28:55 +0200

    built in functions

---
+++ bifs.erl
@@ -0,0 +1,11 @@
+-module(bifs).
+
+% https://erlang.org/doc/man/erlang.html
+
+date(). %Gives date as a tuple
+time(). %Gives time as a tuple
+self(). %Gives pid
+is_tuple("123"). %test object
+length([]). %Gives lenght of list
+
+
