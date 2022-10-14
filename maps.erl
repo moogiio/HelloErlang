665f83bafbb9d34295bc7dd96a64f00ab65d2e19
Peter Tvingmark <tvingmark@gmail.com>
2022-10-14 17:00:48 +0200

    Added maps examples

---
+++ maps.erl
@@ -0,0 +1,14 @@
+% Some example code to use in shell
+% 
+% 1. Scores = #{ "Adam" => 10, "John" => 20 }.
+% 2. NewScores = Scores#{ "Pete" => 30}.
+% 3. UpdatedScore = NewScores#{ "Pete" := 1337 }. (Obs, := operator)
+% 4. TupleScores = maps:to_list(UpdatedScores).
+% 5. OriginalScores = maps:from_list(TupleScores).
+% 6. SizeOfScores = maps:size(OriginalScores).
+% 7. AdamScore = maps.get("Adam", OriginalScores).
+% 8. FindScore = maps:find("Adam", OriginalScores.
+% 9. AllKeys = maps:keys(OriginalScores).
+% 10. RemoveScore = maps:remove("Adam", OriginalScores).
+% 11. ScoreWitout = maps:without(["Adam", "John"], RemoveScore).
+% 
